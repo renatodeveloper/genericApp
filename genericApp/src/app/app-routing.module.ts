c447b4637f2c95b40fcdfe259bd973f01199e305
renatodeveloper <renato_developer@hotmail.com>
2018-09-25 16:32:13 -0300

    all commit

--- genericApp/src/app/app-routing.module.ts
+++ genericApp/src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'authentication', loadChildren: './pages/login/authentication/authentication.module#AuthenticationPageModule' },
+  { path: 'register', loadChildren: './pages/login/register/register.module#RegisterPageModule' },
 ];
 
 @NgModule({
